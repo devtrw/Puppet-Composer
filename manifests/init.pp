7c64fdced7513aafd51d4bcf521515d491378256
Steven Nance <steven@devtrw.com>
2013-01-23 08:31:29 -0800

    Updated order in init manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,17 +6,17 @@ class composer ($installLocation) {
 
     Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+    package { ["php", "curl"]: ensure => "present" }
+    ->
     exec { "install-composer-phar":
         command => "curl -s https://getcomposer.org/installer | php",
         cwd     => $installLocation,
-        creates => "${installLocation}/composer.phar",
-        require => [ Package["php"], Package["curl"] ]
+        creates => "${installLocation}/composer.phar"
     }
-
+    ->
     exec { "update-composer-vendors":
         command => "php composer.phar install",
         cwd     => $installLocation,
-        require => [ Exec["install-composer-phar"] ],
         creates => "$installLocation/vendor/",
         timeout => 0
     }
